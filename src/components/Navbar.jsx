001ec78c16bdb8676d05c53327687c0b49adcb8b
Hridoy <th.hridoy2013@gmail.com>
2023-09-07 13:29:37 +0200

    Fixed the card component, used firestore
    Isolated the individual card component
    from the series. Created a firestore **There is a problem as i amtrying to add saved items as for movies, they have title and as for shows it just has a name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,18 @@ import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 const Navbar = () => {
-  const {user, logOut } =UserAuth();
-  const navigate=useNavigate();
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
 
-  
-  const signTheUserOut=async()=>{
-    try{
+  const signTheUserOut = async () => {
+    try {
       await logOut();
-      navigate('/');
+      navigate("/");
+    } catch (error) {
+      console.log(error);
     }
-    catch(error){
-      console.log(error)
-    }
-  }
-  
+  };
+
   return (
     <div className="flex items-center justify-between p-4 z-[100] absolute w-full">
       <Link to="/">
@@ -24,38 +22,32 @@ const Navbar = () => {
         </h1>
       </Link>
       <div className="flex justify-center items-center">
-      {user?.email ?
-      (
+        {user?.email ? (
           <button className="text-white pr-4">Dashboard</button>
-       )
-      :
-      (
-        <Link to="/signin">
-        <button className="text-white pr-4">Sign In</button>
-      </Link>
-      )
-
-      }
-      <div>
-        
-      {user?.email ?
-      (
-        <Link to="/">
-        <button onClick={signTheUserOut} className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white shadow-lg ">
-          LOGOUT
-        </button>
-      </Link>
-      ):
-      ( <Link to="/signup">
-      <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white shadow-lg ">
-        Sign Up
-      </button>
-    </Link>)
-    }
-         
+        ) : (
+          <Link to="/signin">
+            <button className="text-white pr-4">Sign In</button>
+          </Link>
+        )}
+        <div>
+          {user?.email ? (
+            <Link to="/">
+              <button
+                onClick={signTheUserOut}
+                className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white shadow-lg "
+              >
+                Logout
+              </button>
+            </Link>
+          ) : (
+            <Link to="/signup">
+              <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white shadow-lg ">
+                Sign Up
+              </button>
+            </Link>
+          )}
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 };
